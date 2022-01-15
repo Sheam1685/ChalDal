f1b0d3a96cb5c5b8c5c76046a929e209a4335b08
ansakib <1705107@ugrad.cse.buet.ac.bd>
2022-01-16 01:55:04 +0600

    customer and seller signup works

--- ChalDal/registration/urls.py
+++ ChalDal/registration/urls.py
@@ -3,5 +3,6 @@ from registration import views
 
 urlpatterns = [
     path('signup/', views.returnSignUp, name="signup"),
+    path('seller_signup/', views.returnSellerSignUp, name="seller_signup"),
     path('custlist/', views.returnCustomerList, name="custlist"),
 ]
--- ChalDal/registration/views.py
+++ ChalDal/registration/views.py
@@ -1,3 +1,5 @@
+import email
+import re
 from sqlite3 import Cursor
 from unittest import result
 from django.shortcuts import render
@@ -6,7 +8,74 @@ import datetime
 
 # Create your views here.
 def returnSignUp(request):
+
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        dob = request.POST.get('dob')
+        phn = request.POST.get('phn')
+        address = request.POST.get('address')
+        email = request.POST.get('email')
+        password = request.POST.get('pass')
+
+        cursor = connection.cursor()
+        sql = 'SELECT MAX(CUSTOMER_ID) FROM CUSTOMER'
+        cursor.execute(sql)
+        
+        last_id = cursor.fetchone()
+        last_id = last_id[0]
+        if last_id is not  None:
+            user_id = last_id + 1
+        else:
+            user_id = 1
+
+        cursor = connection.cursor()
+        sql = "INSERT INTO CUSTOMER VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+        
+        cursor.execute(sql,[user_id, fname, lname, address, phn, dob, email,password])
+        connection.commit()
+        cursor.close()
+
+        return render(request, 'homeApp/home_page.html')
+
+        #print(user_id, fname, lname,  address, phn, dob, email,password)
+
+
     return render(request, 'registration/signup.html')
+
+def returnSellerSignUp(request):
+
+    if request.method == 'POST':
+        
+        name = request.POST.get('name')
+        phn = request.POST.get('phn')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        website = request.POST.get('website')
+        password = request.POST.get('pass')
+
+        cursor = connection.cursor()
+        sql = 'SELECT MAX(SELLER_ID) FROM SELLER'
+        cursor.execute(sql)
+        
+        last_id = cursor.fetchone()
+        last_id = last_id[0]
+        if last_id is not  None:
+            user_id = last_id + 1
+        else:
+            user_id = 1
+
+        cursor = connection.cursor()
+        sql = "INSERT INTO SELLER VALUES(%s,%s,%s,%s,%s,%s,%s)"
+        
+        cursor.execute(sql,[user_id, name, address, phn, email,website, password])
+        connection.commit()
+        cursor.close()
+
+        return render(request, 'homeApp/home_page.html')
+
+
+    return render(request, 'registration/seller_signup.html')
 
 def returnCustomerList(request):
 
